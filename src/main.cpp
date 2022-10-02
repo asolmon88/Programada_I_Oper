51ae5f6874b49a123b11d8d02aa58ae27759e270
JOSUE.RETANARODRIGUEZ <josue.retanarodriguez@ucr.ac.cr>
2022-10-02 13:26:58 -0600

    Agrega obtencion de files y división de trabajo

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,52 @@
+#include <sys/types.h>
+#include <filesystem>
 #include <iostream>
+#include <string.h>
+#include <stdio.h>
 #include "Buzon.hpp"
 #include "Reader.hpp"
 
 using namespace std;
 
-int main() {
-  char message[MAX_LENGTH];
-  char result[MAX_LENGTH];
-  
-  memset(message,0, MAX_LENGTH);
-  memset(result,0, MAX_LENGTH);
-  message[0] = 'H';
-  message[1] = 'o';
-  message[2] = 'l';
-  message[3] = 'a';
-  message[4] = '\0';
-  Buzon buzon;
-  buzon.enviar(&message[0]);
-  buzon.recibir(&result[0]);
-  cout << "RECIBIDO: ";
-  for (auto x : result) {
-    cout << x;
-  }
-  cout << endl;
-  buzon.destructor();
-  //Reader::check_errors("file.txt");
+#define NAME_FILE_SIZE 5
 
+int main() {
+	// Obtiene y divide nombres de archivos
+	Buzon buzon;
+	int files_count;
+
+  char file_name[MAX_LENGTH];
+	char result[MAX_LENGTH_RESULT];
+	
+	memset(file_name,0, MAX_LENGTH);
+	memset(result,0, MAX_LENGTH_RESULT);
+
+	/* SEND LIST OF FILES TO BUZON */
+	std::string path = "./files/";
+	// Generates a string based on name of file
+	for (const auto & entry : filesystem::directory_iterator(path)) {
+		files_count++;
+		string file_string = filesystem::path(entry.path()).stem().string();
+		// Converts string to char
+		for (int iterator = 0; iterator < NAME_FILE_SIZE; iterator++) {
+			file_name[iterator] = file_string[iterator];
+    }
+		buzon.enviar(&file_name[0]);
+	}
+	cout << "A total of '" << files_count <<  "' files have been detected..." << endl;
+
+	/* CREATE CHILD PROCCESSES AND DISTRIBUTE WORK */
+	for(int i=0; i < files_count; i++) {
+		// Work
+		if(fork() == 0) {
+			buzon.recibir(&result[0]); // recibe nombre de file
+			string file_name_string = result;
+			cout << "Nombre del file: " << file_name_string << endl;
+			// Reader::check_errors(result); // procesa file
+			cout << "A file has been processed..." << endl;; // sale
+			exit(0);
+		}
+  }
+	buzon.destructor();
   return 0;
 }
